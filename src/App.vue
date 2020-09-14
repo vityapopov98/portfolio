bdd4f8e42185e2988cc9c9d166964944e0ca0454
vityapopov98 <vityapopov98@yandex.ru>
2020-09-14 11:41:43 +0400

    portfolio first commit

---
+++ src/App.vue
@@ -0,0 +1,355 @@
+<template>
+  <div id="app">
+       <nav class="navbar ">
+            <!-- <div class="logo">
+                <img src="images/nike-logo.png" alt="" srcset="">
+            </div> -->
+            <div class="right-menu-small">
+                <div class="menu-btn" @click="openMenu()">
+                        <i class="material-icons">
+                            menu
+                            </i>
+                </div>
+            </div>
+
+            <div class="links menu-hidden" :class="{'links menu-hidden': !isOpen, 'links menu-open': isOpen}">
+                <a href="">Дизайн</a>
+                <a href="">Разработка</a>
+                <a href="">Сувениры</a>
+                <a href="">Контакты</a>
+                <a href="#signin"  class="signin">Вход</a>
+                <div class="hide-menu-btn" @click="closeMenu()">
+                   
+                        <i class="material-icons">
+                            close
+                            </i>
+                    
+                </div>
+            </div>
+        </nav>
+
+
+    <HelloWorld msg="Welcome to Your Vue.js App" v-show="!isOpen" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      isOpen: false
+    }
+  },
+  methods:{
+   openMenu(){
+       this.isOpen = true
+   },
+   closeMenu(){
+       this.isOpen = false
+   }
+  }
+}
+</script>
+
+<style>
+html, body{
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', 'sans-serif';
+}
+
+@media(prefers-color-scheme: dark){
+    body{
+      color: white !important;
+      background-color: #111111;
+    }
+    .navbar{
+        background-color: #111111 !important;
+        display: flex;
+        overflow: hidden;
+        border-bottom: 1px solid #636363;
+        
+        z-index: 111;
+    }
+    .links{
+        background-color: #111111 !important;
+        color: white;
+    }
+    .links a{
+        color: white !important;
+    }
+  }
+  @media(prefers-color-scheme: light){
+      .navbar{
+        border-bottom: 1px solid #e8e8e8;
+    }
+  }
+/* верхний навбар */
+.material-icons{
+    font-size: 1.7rem;
+}
+/* .column-left{
+    width: 50%;
+}
+.column-right{
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+}
+.column-left a, .column-right a{
+    font-family: 'Roboto Condensed', sans-serif;
+    color: #949494;
+    text-decoration: none; 
+}
+.column-left a{
+    border-right: 1px solid #cdcdcd;
+    padding: 1rem;
+    font-weight: 200;
+    font-size: .9rem;
+}
+.column-right a{
+    font-weight: 200;
+    font-size: .9rem;
+    padding-left: 1rem;
+    
+} */
+/* главный навбар */
+
+
+.navbar{
+    background-color: white;
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+    box-sizing: border-box;;
+    z-index: 111;
+    position: fixed;
+}
+.navbar img{
+    width: 4rem;
+    height: 1.5rem;
+}
+.navbar a{
+    display: inline-block;
+    padding: 0 1rem;
+    color: black;
+    text-decoration: none;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-weight: 400;
+}
+.logo{
+    padding: 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    width: 15rem;
+}
+.links{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-grow: 3;
+}
+.search{
+    display: flex;
+    justify-content: flex-end;
+    padding: 0 1rem;
+    width: 15rem;
+}
+.search-input input:focus{
+    width: 11rem;
+    transition: .5s;
+}
+.search-input{
+    border: 1px solid #cdcdcd;
+    display: flex;
+    align-items: center;
+    padding-left: .5rem;
+    margin: .5rem 0;
+}
+.navbar input{
+    padding: .7rem;
+    width: 9rem;
+    border: none;
+    outline: none;
+}
+.links a{
+    line-height: 4rem;
+}
+.links a:hover{
+    line-height: 60px;
+    border-top: 2px solid rgb(255, 255, 255);
+    border-bottom: 2px solid black;
+}
+
+/* __________Просто стили__________ */
+.container{
+    width: 90%;
+    box-sizing: border-box;
+    margin: auto;
+}
+.row{
+    padding-top: 1rem;
+    display: flex;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+
+/* кнопки для навбара в мобильном режиме */
+.hide-menu-btn{
+    display: none;
+}
+.menu-btn{
+    display: none;
+}
+a.signin{
+    display: none;
+}
+a.cart{
+    display: none;
+}
+.search-btn{
+    display: none;
+}
+/* ______Адаптивность_____ */
+
+@media (max-width: 950px){
+    .navbar{width: 100%;}
+    .right-menu-small{
+        display: flex;
+        justify-content: flex-end;
+        flex-grow: 1;
+        padding: .5rem;
+    }
+     /* кнопки для навбара */
+     a.signin{
+        display: block;
+    }
+    a.cart{
+        display: block;
+    }
+    .search-btn{
+        display: block;
+    }
+
+    /* навбар */
+    .menu-btn{
+        display: block;
+    }
+    .hide-menu-btn{
+        display: block;
+        position: absolute;
+        top: 25px;
+        right: 50px;
+    }
+    .menu-hidden{
+        right: -100%;
+        transition: .7s;
+    }
+    .menu-open{
+        right: 0;
+        transition: .7s;
+    }
+    .hide-banner{
+        visibility: hidden;
+        right: 100%;
+        transition: .7s;
+    }
+    .show-banner{
+        visibility: visible;
+        right: 0;
+        transition: .7s;
+    }
+    .links{
+        position: fixed;
+        height: 100vh;
+        width: 100%;
+        background-color: rgb(255, 255, 255);
+        display: flex;
+        flex-direction: column;
+    }
+    .links a{
+        display: flex;
+        justify-content: center;
+        width: 7rem;
+        padding: .5rem 0;
+        line-height: 2rem;
+    }
+    .links a:hover{
+        line-height: 28px;
+    }
+    .navbar-top{
+        display: none;
+    }
+
+
+    .search{
+        display: none;
+    }
+    .hide-menu-btn{
+        position: absolute;
+        right: 50px;
+        top: 25px;
+    }
+
+
+    .cart{
+        display: block;
+    }
+}
+
+@media(max-width: 600px){
+
+    /* two columns */
+    .row{
+        flex-wrap: wrap;
+        padding-top: 1rem;
+    }
+    .column{
+        width: 100%;
+    }
+    .img-container{
+        width: 100%;
+        height: 50vh;
+    }
+    .two-column-section .column.ma-l, .column.ma-r{
+        margin: .2em 0em;
+    }
+    /* Каталог */
+    .catalog{
+        flex-wrap: wrap;
+        width: 70%;
+    }
+    .shoes, .clothes, .children, .suggestions{
+        width: 100%;
+    }
+    /* footer */
+    .column-footer{
+        width: 100%;
+    }
+    .social-media{
+        justify-content: flex-start;
+    }
+    .column{
+        width: 100%;
+    }
+    .column-right{
+        width: 100%;
+        display: block;
+    }
+    .column-right a{
+        display: block;
+        padding: .3rem 0;
+    }
+    footer .column a{
+        padding: .3rem 1rem .3rem 0;
+    }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="row">
+        <div class="banner">
+          <div class="banner-header">
+            <!-- <h1> Hello, my name is Victor.<br> I'm a graphic designer and web develper</h1> -->
+            <h1> Привет, меня зовут Виктор.<br> Я дизайнер и веб разработчик</h1>
+            <!-- <p>Web development, Graphic Design, UX/UI</p> -->
+          </div>
+          <!-- <img :src="require(`@/assets/banner-portfolio.png`)" alt=""> -->
+          <!-- <div class="banner-image"></div> -->
+        </div>
+      </div>
+      <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magnam minus iusto inventore, qui voluptatem, natus suscipit ullam fugiat corrupti vitae et dicta. Provident adipisci atque illum voluptatum debitis cumque?</p> -->
+        <div class="row" >
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Разработка, Дизайн</p>
+                <h2>Coffee in hobby</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-coffee.png`)" alt="">
+                <!-- <img :src="require(`@/assets/portfolio-events.png`)" alt=""> -->
+              </div>
+            </div>
+          </div>
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Разработка</p>
+                <h2>Концепт сайта</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-sanchez.png`)" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Разработка, Дизайн</p>
+                <h2>Умный дом</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-greenzone.png`)" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row" >
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Разработка, Дизайн</p>
+                <h2>Приложение для мероприятий</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-events.png`)" alt="">
+                <!-- <img :src="require(`@/assets/portfolio-events.png`)" alt=""> -->
+              </div>
+            </div>
+          </div>
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Дизайн</p>
+                <h2>Леттеринг коллекция</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-lettering.png`)" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="column-3">
+            <div class="work-block">
+              <div class="work-block-header">
+                <p>Разработка, Дизайн</p>
+                <h2>Умный дом</h2>
+              </div>
+              <div class="work-block-image">
+                <img :src="require(`@/assets/portfolio-coffee.png`)" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          CodeDesign 2020 &copy; All rights reserved
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  @media(prefers-color-scheme: dark){
+      .banner{
+        background-color: black;
+
+      }
+      .banner-image{
+        /* background: url('~@/assets/banner-portfolio-dark.png') center bottom no-repeat; */
+        background-size: contain;
+      }
+  }
+  @media(prefers-color-scheme: light){
+      .banner{
+        background-color: rgb(242, 242, 247);
+      }
+      .banner-image{
+        /* background: url('~@/assets/banner-portfolio-light.png') center bottom no-repeat; */
+        background-size: contain;
+      }
+  }
+  .banner{
+    padding: 5rem 0  0 0;
+    width: 100%;
+    /* height: 80vh; */
+    height: 50vh  ;
+    font-family: 'Roboto Mono', monospace;
+    /* background: url('~@/assets/luca-micheli-ruWkmt3nU58-unsplash.jpg') center center no-repeat ;
+    background-size: cover; */
+  }
+  
+  .banner-header{
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 10rem;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+  .banner-header h1{
+    width: 80%;
+    margin: auto;
+  }
+  .banner>img{
+    object-fit: contain;
+    object-position: bottom;
+    width: 100%;
+    height: 100%;
+  }
+  .banner-image{
+    width: 100%;
+    height: 100%;
+    /* background: url('~@/assets/banner-portfolio.png') center bottom no-repeat;
+    background: url('~@/assets/banner-portfolio-light.png') center bottom no-repeat;
+    background-size: contain; */
+  }
+
+/* Work block */
+
+.column-3{
+  width: 33.33%;
+  box-sizing: border-box;
+  
+}
+.column-3:nth-child(even){
+  margin: 0 1rem 0 1rem;
+}
+.work-block{
+  position: relative;
+}
+.work-block-header{
+  position: absolute;
+  bottom: 1.5rem;
+  left: 1.5rem;
+  color: white;
+}
+.work-block-header>h2{
+  margin: .5rem 0;
+}
+.work-block-header>p{
+  margin: 0;
+}
+.work-block-image{
+  /* width: 100%; */
+  height: 100%;
+  box-sizing: border-box;
+}
+.work-block-image>img{
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+
+  @media(max-width:640px){
+    .banner{
+      /* height: 70vh; */
+      height: 50vh;
+    }
+    .banner-header{
+      top: 7rem;
+    }
+    .banner-image{
+      /* background: url('~@/assets/shotsnapp-1596389440.202.png') center bottom no-repeat; */
+      /* background-size: contain; */
+      background: none;
+    }
+    .column-3{
+      width: 100%;
+      margin: .5rem 0;
+    }
+    .column-3:nth-child(even){
+      margin: 0;
+    }
+    .column-3:last-child{
+      margin-bottom: 0;
+    }
+  }
+
+</style>

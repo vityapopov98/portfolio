fc9de653e65a6121cc4ecda12a5783a6bedfd61a
Victor <vityapopov98@yandex.ru>
2022-05-15 08:44:26 +0300

    Добавлено отображение аудио, и картинок с CDN. мальнькое улучшение css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,35 +8,47 @@
             <!-- <img :src="require(`@/assets/me.png`)" class="banner-avatar" alt=""> -->
             <div class="banner-avatar"></div>
             <!-- <span class="banner-logo">👨🏼‍💻 </span> -->
-            <h1>Привет, меня зовут Виктор.<br> <span class="desktop-header">Я дизайнер и веб разработчик</span></h1>
-            <p>Могу сделать вам сайт, создать логотип и фирменный дизайн. Открыт для связи в социальных сетях и по почте. <br>А пока, можете посмотреть на мои работы.</p>
+            <h1>
+              Библейские заметки<br />
+              <span class="desktop-header"></span>
+            </h1>
+            <p>
+              Тут вы можете найти воскресные проповеди, вдохновляющие цитаты и
+              другие полезные заметки.
+            </p>
             <!-- <a href="https://api.instagram.com/direct/t/340282366841710300949128137420883657077">Заказать</a> -->
             <!-- <p>Web development, Graphic Design, UX/UI</p> -->
           </div>
-            <!-- <a href="" class="banner-btn">Contact me</a> -->
+          <!-- <a href="" class="banner-btn">Contact me</a> -->
           <!-- <img :src="require(`@/assets/jeremy-bishop-G9i_plbfDgk-unsplash.jpg`)" alt=""> -->
           <!-- <div class="banner-image"></div> -->
         </div>
       </div>
 
-      <div class="row" v-for="(projects, index) in projectsDoubleArray" :key="index">
+      <div
+        class="row"
+        v-for="(projects, index) in projectsDoubleArray"
+        :key="index"
+      >
         <div class="column-3" v-for="(project, index) in projects" :key="index">
           <div class="work-block">
             <div class="work-block-header">
-              <p>{{project.category}}</p>
-              <router-link :to="{name: 'Project', params: {name: project.urlName}}">
-                <h2>{{project.name}}</h2>
+              <p>{{ project.category }}</p>
+              <router-link
+                :to="{ name: 'Project', params: { name: project.urlName } }"
+              >
+                <h2>{{ project.name }}</h2>
               </router-link>
             </div>
             <div class="work-block-image">
-                <img :src="require(`@/assets/${project.headerImg}`)" alt="">
-              </div>
+              <!--<img :src="require(`@/assets/${project.headerImg}`)" alt="" /> -->
+              <img :src="project.headerImg" alt="" />
+            </div>
           </div>
         </div>
-
       </div>
 
-        <!-- <div class="row" >
+      <!-- <div class="row" >
           <div class="column-3">
             <div class="work-block">
               <div class="work-block-header">
@@ -78,7 +90,7 @@
           </div>
         </div> -->
 
-        <!-- <div class="row" >
+      <!-- <div class="row" >
           <div class="column-3">
             <div class="work-block">
               <div class="work-block-header">
@@ -119,7 +131,6 @@
             </div>
           </div>
         </div> -->
-
     </div>
 
     <!-- <footer>
@@ -128,7 +139,6 @@
           <div class="col-md-6 copyright">CodeDesign 2020 &copy; All rights reserved</div>
           <div class="col-md-6">
             <div class="footer-links-block">
-              <a href="https://www.instagram.com/mbm_and_pline/" class="footer-link">Instagram</a>
               <a href="https://vk.com/vitrekp" class="footer-link">Vk</a>
               <a href="https://www.behance.net/vityapopovdd27" class="footer-link">Behance</a>
               <a href="https://github.com/vityapopov98" class="footer-link">GitHub</a>
@@ -139,334 +149,332 @@
         </div>
       </div>
     </footer> -->
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 
 export default {
-  name: 'Home',
-  components:{
-    Footer
+  name: "Home",
+  components: {
+    Footer,
   },
   props: {
-    msg: String
+    msg: String,
   },
-  data: function(){
+  data: function() {
     return {
-      projectsDoubleArray: []
-    }
+      projectsDoubleArray: [],
+    };
   },
-  methods:{
-    loadProjects(){
+  methods: {
+    loadProjects() {
       // this.projects = this.$root.projects;
       var tempArr = [];
 
-      this.$root.projects.forEach(element => {
-        console.log(tempArr)
-        tempArr.push(element)
-        if(tempArr.length == 3){
-          this.projectsDoubleArray.push(tempArr)
-          tempArr = []
+      //Распределение всех записей в double array. элемент это 3 проекта в строке.
+      this.$root.projects.forEach((element) => {
+        console.log(tempArr);
+        tempArr.push(element);
+        if (tempArr.length == 3) {
+          this.projectsDoubleArray.push(tempArr);
+          tempArr = [];
           // tempArr.splice(0, tempArr.length)
         }
       });
-    }
+    },
   },
-  mounted(){
-    this.loadProjects()
-    console.log('he', this.projectsDoubleArray)
-  }
-}
+  mounted() {
+    this.loadProjects();
+    console.log("he", this.projectsDoubleArray);
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-  @media(prefers-color-scheme: dark){
-      .banner{
-        background-color: black;
-
-      }
-      .banner-image{
-        /* background: url('~@/assets/banner-portfolio-dark.png') center bottom no-repeat; */
-        background-size: contain;
-      }
-      h1{
-        color: #f5f5f7;
-      }
-      .banner-header>p{
-        color: #86868b;
-      }
-      .banner-avatar{
-        width: 200px;
-        height: 200px;
-        margin: auto;
-        padding: 0%;
-        background: url('~@/assets/me-black.png') center bottom no-repeat;
-        background-size: contain;
-      }
-      .footer-link{
-        color: #4d4d4d;
-        margin: 0 1rem;
-      }
-      .footer-link:hover{
-        color: #6d6d6d;
-      }
+@media (prefers-color-scheme: dark) {
+  .banner {
+    background-color: black;
   }
-  @media(prefers-color-scheme: light){
-      .banner{
-        background-color: rgb(242, 242, 247);
-      }
-      .banner-image{
-        /* background: url('~@/assets/banner-portfolio-light.png') center bottom no-repeat; */
-        background-size: contain;
-      }
-      h1{
-        color: #1d1d1f;
-      }
-      .banner-header>p{
-        color: #8c8c8c;
-      }
-      .banner-btn{
-        border: 1px solid #8c8c8c;
-        border-radius: 25px;
-        padding: .5rem 1rem;
-        height: 20px;
-      }
-      .banner-avatar{
-        background: url('~@/assets/me.png') center bottom no-repeat;
-        background-size: contain;
-      }
-      .footer-link{
-        color: #9c9c9c;
-        margin: 0 1rem;
-      }
-      .footer-link:hover{
-        color: #bebebe;
-      }
+  .banner-image {
+    /* background: url('~@/assets/banner-portfolio-dark.png') center bottom no-repeat; */
+    background-size: contain;
   }
+  h1 {
+    color: #f5f5f7;
+  }
+  .banner-header > p {
+    color: #86868b;
+  }
+  .banner-avatar {
+    width: 200px;
+    height: 200px;
+    margin: auto;
+    padding: 0%;
+    background: url("~@/assets/bible.png") center bottom no-repeat;
+    background-size: contain;
+  }
+  .footer-link {
+    color: #4d4d4d;
+    margin: 0 1rem;
+  }
+  .footer-link:hover {
+    color: #6d6d6d;
+  }
+}
+@media (prefers-color-scheme: light) {
+  .banner {
+    background-color: rgb(242, 242, 247);
+  }
+  .banner-image {
+    /* background: url('~@/assets/banner-portfolio-light.png') center bottom no-repeat; */
+    background-size: contain;
+  }
+  h1 {
+    color: #1d1d1f;
+  }
+  .banner-header > p {
+    color: #8c8c8c;
+  }
+  .banner-btn {
+    border: 1px solid #8c8c8c;
+    border-radius: 25px;
+    padding: 0.5rem 1rem;
+    height: 20px;
+  }
+  .banner-avatar {
+    background: url("~@/assets/bible.png") center bottom no-repeat;
+    background-size: contain;
+  }
+  .footer-link {
+    color: #9c9c9c;
+    margin: 0 1rem;
+  }
+  .footer-link:hover {
+    color: #bebebe;
+  }
+}
 
 /* --------- Баннер ------- */
-  .banner-avatar{
-        width: 200px;
-        height: 200px;
-        margin: auto;
-        padding: 0%;
-      }
-  a.banner-btn{
-        border: 1px solid #8c8c8c;
-        border-radius: 20px;
-        padding: .5rem;
-        color: #8c8c8c;
-        text-decoration: none;
-        justify-content: left;
-      }
-  .banner{
-    padding: 5rem 0  0 0;
-    width: 100%;
-    height: 80vh;
-    /* height: 50vh  ; */
-    /* font-family: 'Roboto Mono', monospace; */
-    font-family: 'Montserrat';
-    /* background: url('~@/assets/luca-micheli-ruWkmt3nU58-unsplash.jpg') center center no-repeat ;
+.banner-avatar {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  padding: 0%;
+}
+a.banner-btn {
+  border: 1px solid #8c8c8c;
+  border-radius: 20px;
+  padding: 0.5rem;
+  color: #8c8c8c;
+  text-decoration: none;
+  justify-content: left;
+}
+.banner {
+  padding: 5rem 0 0 0;
+  width: 100%;
+  height: 50vh;
+  /* height: 50vh  ; */
+  /* font-family: 'Roboto Mono', monospace; */
+  font-family: "Montserrat";
+  /* background: url('~@/assets/luca-micheli-ruWkmt3nU58-unsplash.jpg') center center no-repeat ;
     background-size: cover; */
-    display: flex;
-  }
-  .banner-logo{
-      width: 90%;
-      margin: auto;
-      font-size: 150px;
-  }
-  .banner-header{
-    position: absolute;
-    margin-left: auto;
-    margin-right: auto;
-    top: 7rem;
-    left: 0;
-    right: 0;
-    text-align: center;
-  }
-  .banner-header h1{
-    font-size: 54px;
-    width: 70%;
-    margin: auto;
-  }
-  .banner-header>p{
-    font-family: 'Montserrat';
-    font-weight: 500;
-    font-size: 24px;
-    padding-top: 2rem;
-    width: 60%;
-    margin: auto;
-    
-  }
-  .banner>img{
-    object-fit: cover;
-    object-position: bottom;
-    width: 100%;
-    height: 100%;
-  }
-  .banner-image{
-    width: 100%;
-    height: 100%;
-    /* background: url('~@/assets/banner-portfolio.png') center bottom no-repeat;
+  display: flex;
+}
+.banner-logo {
+  width: 90%;
+  margin: auto;
+  font-size: 150px;
+}
+.banner-header {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  top: 7rem;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.banner-header h1 {
+  font-size: 54px;
+  width: 70%;
+  margin: auto;
+}
+.banner-header > p {
+  font-family: "Montserrat";
+  font-weight: 500;
+  font-size: 24px;
+  padding-top: 2rem;
+  width: 60%;
+  margin: auto;
+}
+.banner > img {
+  object-fit: cover;
+  object-position: bottom;
+  width: 100%;
+  height: 100%;
+}
+.banner-image {
+  width: 100%;
+  height: 100%;
+  /* background: url('~@/assets/banner-portfolio.png') center bottom no-repeat;
     background: url('~@/assets/banner-portfolio-light.png') center bottom no-repeat;
     background-size: contain; */
-  }
+}
 
 /* ----- Конец Банера ------- */
 
 /* Work block */
 
-.column-3{
+.column-3 {
   width: 33.33%;
   box-sizing: border-box;
-  
 }
-.column-3:nth-child(even){
+.column-3:nth-child(even) {
   margin: 0 1rem 0 1rem;
 }
-.work-block{
+.work-block {
   position: relative;
+  height: 350px;
 }
-.work-block-header{
+.work-block-header {
   position: absolute;
   bottom: 1.5rem;
   left: 1.5rem;
   color: white;
 }
-.work-block-header>h2{
-  margin: .5rem 0;
+.work-block-header > h2 {
+  margin: 0.5rem 0;
 }
-.work-block-header>p{
+.work-block-header > p {
   margin: 0;
 }
-.work-block-image{
+.work-block-image {
   /* width: 100%; */
   height: 100%;
   box-sizing: border-box;
 }
-.work-block-image>img{
+.work-block-image > img {
   object-fit: cover;
   width: 100%;
   height: 100%;
 }
 
-footer{
+footer {
   padding: 3rem 0;
   color: #4d4d4d;
 }
-.col-md-6{
-  
+.col-md-6 {
   width: 50%;
   box-sizing: border-box;
 }
-.footer-links-block{
+.footer-links-block {
   text-align: right;
-  width: 100%; 
+  width: 100%;
 }
-.copyright{
+.copyright {
   display: block;
 }
 
-@media(max-width:1200px){
-  .banner-header h1{
+@media (max-width: 1200px) {
+  .banner-header h1 {
     font-size: 42px;
   }
-  .banner-header p{
+  .banner-header p {
     font-size: 20px;
   }
 }
-@media(max-width:920px){
-  .banner-header h1{
+@media (max-width: 920px) {
+  .banner-header h1 {
     font-size: 38px;
   }
-  .banner-header p{
+  .banner-header p {
     font-size: 17px;
   }
 }
 /* iPhone SE 1 */
-@media(max-width:320px){
-    .banner-avatar{
-        width: 150px;
-        height: 150px;
-        margin: auto;
-        padding: 0%;
-      }
-  .banner-header h1{
+@media (max-width: 320px) {
+  .banner-avatar {
+    width: 150px;
+    height: 150px;
+    margin: auto;
+    padding: 0%;
+  }
+  .banner-header h1 {
     font-size: 30px;
   }
-  .banner-header p{
+  .banner-header p {
     font-size: 14px;
   }
 }
 /* iPad pro */
-@media(min-height:1366px){ 
-  .banner{
+@media (min-height: 1366px) {
+  .banner {
     height: 40vh;
   }
 }
-@media(min-height:1040px){ 
-  .banner{
+@media (min-height: 1040px) {
+  .banner {
     height: 60vh;
   }
 }
-  @media(max-width:640px){
-    .banner{
-      /* height: 70vh; */
-      height: 80vh;
-      /* background: none; */
-    }
-    .banner-header{
-      top: 7rem;
-    }
-    .banner-header>h1{
-      width: 80%;
-    }
-    h1{
-      font-size: 32px;
-      /* text-align: left; */
-      font-weight: 500;
-    }
-    .desktop-header{
-        display: none;
-    }
-    .banner-header>p{
-      font-size: 17px;
-      width: 80%;
-      /* text-align: left; */
-    }
-    .banner-image{
-      /* background: url('~@/assets/shotsnapp-1596389440.202.png') center bottom no-repeat; */
-      /* background-size: contain; */
-      background: none;
-    }
-    .column-3{
-      width: 100%;
-      margin: .5rem 0;
-    }
-    .column-3:nth-child(even){
-      margin: 0;
-    }
-    .column-3:last-child{
-      margin-bottom: 0;
-    }
-
-    .footer-links-block{
-      text-align: center;
-      line-height: 32px;
-      display: flex;
-      flex-wrap: wrap;
-      flex-direction: column;
-    }
-    
-    .col-md-6{
-      width: 100%;
-    }
-    .copyright{display: none;}
+@media (max-width: 640px) {
+  .banner {
+    /* height: 70vh; */
+    height: 80vh;
+    /* background: none; */
+  }
+  .banner-header {
+    top: 7rem;
+  }
+  .banner-header > h1 {
+    width: 80%;
+  }
+  h1 {
+    font-size: 32px;
+    /* text-align: left; */
+    font-weight: 500;
+  }
+  .desktop-header {
+    display: none;
+  }
+  .banner-header > p {
+    font-size: 17px;
+    width: 80%;
+    /* text-align: left; */
+  }
+  .banner-image {
+    /* background: url('~@/assets/shotsnapp-1596389440.202.png') center bottom no-repeat; */
+    /* background-size: contain; */
+    background: none;
+  }
+  .column-3 {
+    width: 100%;
+    margin: 0.5rem 0;
+  }
+  .column-3:nth-child(even) {
+    margin: 0;
+  }
+  .column-3:last-child {
+    margin-bottom: 0;
   }
 
+  .footer-links-block {
+    text-align: center;
+    line-height: 32px;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+
+  .col-md-6 {
+    width: 100%;
+  }
+  .copyright {
+    display: none;
+  }
+}
 </style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,160 +1,207 @@
 <template>
-    <div class="project-page">
-        <div class="container-p">
-            <div class="row mt-5">
-                <div class="col-md-12">
-                    <h1>{{projectDetails.name}}</h1>
-                    <p>{{projectDetails.description}}</p>
-                </div>
-            </div>
+  <div class="project-page">
+    <div class="container-p">
+      <div class="row mt-5">
+        <div class="col-md-12">
+          <h1>{{ projectDetails.name }}</h1>
+          <p>{{ projectDetails.description }}</p>
         </div>
-        <div class="project-banner">
-            <!-- <img :src="require(`@/assets/${projectDetails.headerImg}`)" alt="" class="project-banner-img"> -->
-            <img :src="headerImage" alt="" class="project-banner-img">
-        </div>
-        <div class="container-p">
-            
-            <div class="row" v-for="(element, index) in projectDetails.text" :key="index">
-                <div class="col-md-12">
-                    <!-- <h2>{{projectName}}</h2> -->
-                    <img v-if="element.includes('pic:')" :src="require(`@/assets/${element.slice(4)}`)" alt="" class="project-img">
-                    <h2 v-else-if="element.includes('h:')">{{element.slice(2)}}</h2>
-                    <div class="project-link" v-else-if="element.includes('l:')">
-                        <span class="material-icons">
-                            link
-                        </span>
-                        <a :href="getLink(element)" >{{getLinkName(element)}}</a>
-                    </div>
-                    <p v-else >{{element}}</p>
-                    <!-- <p>{{description}}</p> -->
-                </div>
-            </div>
-                
-        </div>
-        
-        <Footer></Footer>
+      </div>
     </div>
-    
+    <div class="project-banner">
+      <!-- <img :src="require(`@/assets/${projectDetails.headerImg}`)" alt="" class="project-banner-img"> -->
+      <!--<img :src="headerImage" alt="" class="project-banner-img" /> -->
+      <img :src="projectDetails.headerImg" alt="" class="project-banner-img" />
+    </div>
+    <div class="container-p">
+      <div
+        class="row"
+        v-for="(element, index) in projectDetails.text"
+        :key="index"
+      >
+        <div class="col-md-12">
+          <!-- <h2>{{projectName}}</h2> -->
+          <img
+            v-if="element.includes('pic:')"
+            :src="require(`@/assets/${element.slice(4)}`)"
+            alt=""
+            class="project-img"
+          />
+          <img
+            v-else-if="element.includes('img:')"
+            :src="element.slice(4)"
+            alt=""
+            class="project-img"
+          />
+          <audio
+            v-else-if="element.includes('audio:')"
+            controls
+            :src="element.slice(6)"
+          >
+            Your browser does not support the audio
+          </audio>
+          <h2 v-else-if="element.includes('h:')">{{ element.slice(2) }}</h2>
+          <div class="project-link" v-else-if="element.includes('l:')">
+            <span class="material-icons">
+              link
+            </span>
+            <a :href="getLink(element)">{{ getLinkName(element) }}</a>
+          </div>
+          <p v-else>{{ element }}</p>
+          <!-- <p>{{description}}</p> -->
+        </div>
+      </div>
+    </div>
+
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
 
 export default {
-  name: 'Project',
+  name: "Project",
   components: {
-      Footer
+    Footer,
   },
-  data: function(){
-      return {
-          projectName: this.$route.params.name,
-          projectImage: {},
+  data: function() {
+    return {
+      projectName: this.$route.params.name,
+      projectImage: {},
 
-            headerImg: '',
-            name: 'Coffee in Hobby',
-            description: '',
-            projectDetails: {},
-            linkName: ''
-      }
+      headerImg: "",
+      name: "Coffee in Hobby",
+      description: "",
+      projectDetails: {},
+      linkName: "",
+    };
   },
-  methods:{
-    loadProject(){
-        // alert(this.projectName)
-        this.$root.projects.forEach(element => {
-            console.log(element.urlName)
-            if(element.urlName == this.projectName){
-                console.log('hurray')
-                this.description = element.description;
-                this.projectImage = element.headerImg;
-                this.projectDetails = element
-                console.log('reer?',this.projectImage)
+  methods: {
+    loadProject() {
+      // alert(this.projectName)
+      this.$root.projects.forEach((element) => {
+        console.log(element.urlName);
+        if (element.urlName == this.projectName) {
+          console.log("hurray");
+          this.description = element.description;
+          this.projectImage = element.headerImg;
+          this.projectDetails = element;
+          // Подготовить прямые ссылки на файлы.
+
+          const article = element.text;
+          article.forEach((paragraph, index) => {
+            if (paragraph.includes("wdLink:")) {
+              console.log(
+                "fethcing link",
+                paragraph.substring(
+                  paragraph.indexOf(":") + 1,
+                  paragraph.length
+                )
+              );
+              fetch(
+                paragraph.substring(
+                  paragraph.indexOf(":") + 1,
+                  paragraph.length
+                ),
+                {
+                  headers: { Accept: "text/html" },
+                }
+              ).then((res) => {
+                // распарсить страницу
+                console.log("wd anser", res);
+                console.log(res.body);
+                article[index] = "wdLink:" + "новая ссылка";
+              });
             }
-        });
-    },
-    getLink(data){
-        if(data.includes('l:')){
-            var columnPosition = data.indexOf(':');
-            return data.slice(columnPosition+1)
+          });
+          console.log("reer?", this.projectImage);
         }
+      });
     },
-    getLinkName(data){
-        var columnPosition = data.indexOf(':');
-        return data.slice(0, columnPosition-1);
-    }
-    
-  },
-  computed:{
-      headerImage(){
-          console.log(this.projectDetails.headerImg)
-          if(this.projectDetails.headerImg != undefined){
-              return require(`@/assets/${this.projectDetails.headerImg}`)
-          }
-          return '' // Почему-то в начале при переходе на страницу vue ругается на header image типо оно undefined, но вообще все работает
-          // написав этот костыль он перестал ругаться
+    getLink(data) {
+      if (data.includes("l:")) {
+        var columnPosition = data.indexOf(":");
+        return data.slice(columnPosition + 1);
       }
+    },
+    getLinkName(data) {
+      var columnPosition = data.indexOf(":");
+      return data.slice(0, columnPosition - 1);
+    },
   },
-  mounted(){
-     this.loadProject()
-  }
-}
+  computed: {
+    headerImage() {
+      console.log(this.projectDetails.headerImg);
+      if (this.projectDetails.headerImg != undefined) {
+        return require(`@/assets/${this.projectDetails.headerImg}`);
+      }
+      return ""; // Почему-то в начале при переходе на страницу vue ругается на header image типо оно undefined, но вообще все работает
+      // написав этот костыль он перестал ругаться
+    },
+  },
+  mounted() {
+    this.loadProject();
+  },
+};
 </script>
 
 <style scoped>
-h1{
-    padding-top: 5rem;
-    font-size: 42px;
+h1 {
+  padding-top: 5rem;
+  font-size: 42px;
 }
-h2{
-    margin: 0;
+h2 {
+  margin: 0;
 }
-p{
-    margin-bottom: 3rem;
-    font-size: 20px;
-    line-height: 36px;
+p {
+  margin-bottom: 3rem;
+  font-size: 20px;
+  line-height: 36px;
 }
-.container-p{
-    padding-top: 2rem;
-    width: 55%;
-    margin: auto;
+.container-p {
+  padding-top: 2rem;
+  width: 55%;
+  margin: auto;
 }
-.project-img{
-    width: 100%;
-    margin: auto;
-    height: auto;
+.project-img {
+  width: 100%;
+  margin: auto;
+  height: auto;
 }
-.project-banner{
-    width: 80%;
-    height: 600px;
-    margin: auto;
-    box-sizing: border-box;
+.project-banner {
+  width: 80%;
+  height: 600px;
+  margin: auto;
+  box-sizing: border-box;
 }
-.project-banner-img{
-    width: 100%;
-    margin: auto;
-    height: 100%;
-    object-fit: cover;
+.project-banner-img {
+  width: 100%;
+  margin: auto;
+  height: 100%;
+  object-fit: cover;
 }
-.project-link{
-    display: flex;
-    align-items: center;
+.project-link {
+  display: flex;
+  align-items: center;
 }
 
-.project-link>a:hover{
-    text-decoration: underline !important;
-    color: #b1b1b1;
+.project-link > a:hover {
+  text-decoration: underline !important;
+  color: #b1b1b1;
 }
-@media(prefers-color-scheme: light){
-    .project-link>a{
-        color: #111111;
-    }
-    .project-link>a:hover{
+@media (prefers-color-scheme: light) {
+  .project-link > a {
+    color: #111111;
+  }
+  .project-link > a:hover {
     text-decoration: underline !important;
     color: #202020;
+  }
 }
-}
-@media(max-width:640px){
-    .container-p{
-        width: 80%;
-    }
+@media (max-width: 640px) {
+  .container-p {
+    width: 80%;
+  }
 }
 </style>
